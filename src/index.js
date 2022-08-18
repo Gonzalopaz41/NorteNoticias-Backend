0b9112aa4dfe65e87db69ac79e63b57b9bfd4245
Gonzalo Paz <gonzalopazok@gmail.com>
2022-08-18 11:56:57 -0300

    routes created

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const app = express();
 require ('dotenv/config');
 const PORT = process.env.PORT;
 const cors = require('cors')
-
-
 app.use(cors())
-
 app.use(express.json());
 
+const adminRoute = require ('./routes/admin.route')
+const loginRoute = require('./routes/login.route')
+const registerRoute = require ('./routes/register.route')
+
+app.use ('/admin',adminRoute)
+app.use('/login',loginRoute)
+app.use ('/register', registerRoute)
 app.listen (PORT, ()=>{
     console.log (`se esta escuchando el puerto ${PORT}`)
 })
