8528139423d32881031e2bc186f081d2f4c9123a
joselascano <ventasgeek1@gmail.com>
2022-09-02 21:48:23 -0300

    registrorutas

--- src/routes/register.route.js
+++ src/routes/register.route.js
@@ -1,11 +1,27 @@
-const express = require ('express');
+const express =require('express');
 const router = express.Router();
+const { postRegister, getUsers } = require('../controllers/register.controller');
+const {body} = require ('express-validator')
+//const {validateEmail} = require('../helpers/validation.js')
+const {errors} = require ('../helpers/validation')
+router.get('/',getUsers);
 
-router.get('/',(req,res)=>{
-
-})
-router.post('/',(req,res)=>{
-
-})
+router.post('/',
+    body('email', 'Ingrese un E-mail valido')
+        .exists()
+        .isEmail(),
+    body('name','Ingrese un nombre')
+        .exists()
+        .isLength({min:2}),
+    body('surname','Ingrese un apellido')
+        .exists()
+        .isLength({min:2}),
+    body('password','La contraseña debe tener un minimo de 3 caracteres')
+        .exists()
+        .isLength({min:3}),
+        (req,res,next)=>{
+            errors(req,res,next)
+        }
+        ,postRegister)
 
 module.exports=router
