cfb87a6fba5b0ed3f57675c582a5acdf7816e2a6
Gonzalopaz41 <gonzalopazok@gmail.com>
2022-09-08 17:29:27 -0300

    middlewares-jwt created and connected with login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,24 +1,36 @@
 const user = require('../models/users')
 const bcrypt = require('bcrypt')
-
+const jwt = require ('jsonwebtoken')
+let match;
 
 const postLogin = async (req,res) =>{
     const {email,password} = req.body
-
     const searchEmail = await user.findOne({email})
+    
     if(searchEmail){
-    const match = bcrypt.compareSync (password,searchEmail.password)
-    if (match){
-        res.status(200).json("usuario logueado")
-        res.json({token: generateJWT()})
-    } else{
-        res.status(401).json("verifica los datos")
-    }
-    }else{
-        return res.status(404).json("verifica los datos")
-    }
+    match = bcrypt.compareSync (password,searchEmail.password)}
     
-    
+        try{
+        if (match){
+            const payload = {
+                id: searchEmail._id,
+                email:searchEmail.email
+            }
+            const token = jwt.sign(payload,process.env.JWT_SECRET,{
+                expiresIn: '7d'
+            })
+            res.status(200).json({msg:"Usuario logueado", token})
+    }
+    else{
+        const error = new Error ("verifica los datos");
+        return res.status(404).json({msg:error.message});
+    }
+
+    } catch(error){
+        res.status(406).json({msg:"verifica los datos",error})
+    }
 }
+    
+
 
 module.exports={postLogin}
